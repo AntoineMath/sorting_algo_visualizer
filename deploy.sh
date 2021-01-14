51387e83aff1f0e7b1a65251f5fd93b391b0a6b6
AntoineMath <antoine.mathurin@ecam-rennes.com>
2021-01-14 19:36:29 +0100

    'master' -> 'main' correction

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:antoinemath/sorting_algo_visualizer.git master:gh-pages
+git push -f git@github.com:antoinemath/sorting_algo_visualizer.git main:gh-pages
 
 cd -
